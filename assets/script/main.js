0aeb657d1c67350ca8bc8a63c368af8d8fc089b7
tokuno-masahiko <tokuno5956@gmail.com>
2024-05-28 21:19:10 +0900

    Q&Aセクション作成のコミット

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,18 +1,38 @@
 $(document).ready(function() {
   let profiles = ['.voice__content__square1', '.voice__content__square2', '.voice__content__square3', '.voice__content__square4', '.voice__content__square5'];
-  let currentIndex = 0;
+  let profileIndex = 0;
+  let indicatorIndex = 0;
 
   function showProfile(index) {
       $('.js-profile').removeClass('js-active');
       $(profiles[index]).addClass('js-active');
   }
+
+  function updateIndicators(index) {
+    $('.voice__indicator--item').removeClass('js-active');
+    $('.voice__indicator--item[data-index=' + index + ']').addClass('js-active');
+  }
+
+  function updateDisplay() {
+    showProfile(profileIndex);
+    updateIndicators(indicatorIndex);
+  }
+
   $('.voice__content--left').click(function() {
-      currentIndex = (currentIndex - 1 + profiles.length) % profiles.length;
-      showProfile(currentIndex);
+    profileIndex = (profileIndex - 1 + profiles.length) % profiles.length;
+    indicatorIndex = (indicatorIndex - 1 + 6) % 6;
+    updateDisplay();
   });
 
   $('.voice__content--right').click(function() {
-      currentIndex = (currentIndex + 1) % profiles.length;
-      showProfile(currentIndex);
+    profileIndex = (profileIndex + 1) % profiles.length;
+    indicatorIndex = (indicatorIndex + 1) % 6;
+    updateDisplay();
+  });
+
+  $('.voice__indicator--item').click(function() {
+    indicatorIndex = $(this).data('index');
+    profileIndex = indicatorIndex % profiles.length;
+    updateDisplay();
   });
 });
