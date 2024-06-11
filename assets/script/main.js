b0443821e568d764cee1cc95765c7074ad44c986
tokuno-masahiko <tokuno5956@gmail.com>
2024-06-11 21:24:37 +0900

    LP2修正完了のコミット

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,48 +1,54 @@
 const swiper = new Swiper(".swiper", {
-  loop: true,
-  // 前後の矢印
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  // スライドの表示枚数：500px未満の場合
-  slidesPerView: 1,
-  breakpoints: {
-    // スライドの表示枚数：500px以上の場合
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 40,
-      slidesPerGroup:3,
+    loop: true,
+    // 前後の矢印
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    // スライドの表示枚数：500px未満の場合
+    slidesPerView: 1,
+    breakpoints: {
+        // スライドの表示枚数：500px以上の場合
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+            slidesPerGroup: 3,
+        }
     }
-  }
+});
+
+$(function(){
+    $('.js_container').click(function(){
+        $(this).find('.js_txt').slideToggle();
+        $(this).find('.js_txt').css('display', 'flex');
+        if ($(this).find('.js_up').css('display') == 'none') {
+            $(this).find('.js_up').css('display', 'block');
+            $(this).find('.js_down').css('display', 'none');
+        } else {
+            $(this).find('.js_up').css('display', 'none');
+            $(this).find('.js_down').css('display', 'block');
+        }
+    });
 });
 
 
-
-for (let i = 1; i <= 5; i++) {
-  $(`.js-container${i}`).click(function() {
-    if ($(`.js-up${i}`).css('display') == 'none') {
-      $(`.js-up${i}`).css('display', 'block');
-      $(`.js-down${i}`).css('display', 'none');
-      $(`.js-txt${i}`).css('display', 'flex');
-    } else {
-      $(`.js-up${i}`).css('display', 'none');
-      $(`.js-down${i}`).css('display', 'block');
-      $(`.js-txt${i}`).css('display', 'none');
-    }
-  });
-}
-document.addEventListener('DOMContentLoaded', (event) => {
-  const image = document.getElementById('scrollToTopImage');
-
-  image.addEventListener('click', () => {
-      window.scrollTo({
-          top: 0,
-          behavior: 'smooth'
-      });
-  });
+$(document).ready(function () {
+    $("#scrollToTopImage").hide();
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 100) {
+            $("#scrollToTopImage").fadeIn("fast");
+        } else {
+            $("#scrollToTopImage").fadeOut("fast");
+        }
+    });
+    $('#scrollToTopImage').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
 });
